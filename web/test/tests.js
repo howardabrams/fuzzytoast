960862361122398c917f6c56986fc3b345e9c9d9
James Wilson <james@jameswilson.name>
2011-08-01 12:25:43 -0500

    removed comment cruft

--- web/test/tests.js
+++ web/test/tests.js
@@ -1,8 +1,3 @@
-// documentation on writing tests here: http://docs.jquery.com/QUnit
-// example tests: https://github.com/jquery/qunit/blob/master/test/same.js
-
-// below are some general tests but feel free to delete them.
-
 module("Fuzzytoast tests");
 
 asyncTest("Population from external template and external data", function() {
